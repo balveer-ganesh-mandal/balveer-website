b578b35ff2776fcd8b96b0d76af66f7b1ca05242
avdhootp14 <avdhootpimparkar14062004@gmail.com>
2026-02-28 15:25:53 +0530

    fix: remove deprecated mongoose connection options

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -7,10 +7,7 @@ const connectDB = async () => {
             return;
         }
 
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
